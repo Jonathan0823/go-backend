ba1b91e8fa868a31f1b37a427f9e566a765ae6ef
Guinn <eguinjonathan08@gmail.com>
2024-12-03 18:59:34 +0700

    Add task package with initial Task model and service structure

---
+++ internal/task/model.go
@@ -0,0 +1,11 @@
+package task
+
+type Task struct {
+	ID          int    `json:"id"`
+	Title 	 	string `json:"title"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+	DueDate	 	string `json:"due_date"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
