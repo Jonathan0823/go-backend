a0880ba1e43165364ddf52a9c9d22b29ef84f5e9
Guinn <eguinjonathan08@gmail.com>
2024-12-03 19:14:44 +0700

    Add repository interface and implementation for task retrieval

--- internal/task/repository.go
+++ internal/task/repository.go
@@ -1,1 +1,35 @@
-package task	
+package task
+
+import "database/sql"
+
+
+type Repository interface {
+	getAll() ([]Task, error)
+}
+
+type repository struct {
+	db *sql.DB
+}
+
+func NewRepository(db *sql.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) getAll() ([]Task, error) {
+	rows, err := r.db.Query("SELECT * FROM tasks")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var tasks []Task
+	for rows.Next() {
+		var task Task
+		if err := rows.Scan(&task.ID, &task.Title, &task.Description, &task.Status, &task.DueDate, &task.CreatedAt, &task.UpdatedAt); err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, task)
+	}
+
+	return tasks, nil
+}
